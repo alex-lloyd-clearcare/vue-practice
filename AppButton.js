d8768d96c93a985b2933fca63c5819d458a666c4
Alex Lloyd <alexlloyd@CC-MACBOOKAIR-XX.local>
2025-02-24 09:18:11 +0000

    updated

--- AppButton.js
+++ AppButton.js
@@ -1,13 +1,16 @@
 export default {
-    template: `
-        <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
-            <slot />
-        </button>
-    `,
+        template: `
+            <button 
+                class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" 
+                :disabled="processing"
+            >
+                hekllo
+            </button>
+        `,
 
-    data() {
-        return {
-            processing: true
-        };
-    }
+        data() {
+            return {
+                processing: false
+            };
+        }
 }
